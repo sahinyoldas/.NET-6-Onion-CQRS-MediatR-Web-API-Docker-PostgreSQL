cb5831833d626e8b090c7d10ee3129f6d455003f
sahinyoldas <sahinyoldas@gmail.com>
2023-08-14 14:48:00 +0100

    infrastructure and presentation folders added

---
+++ Infrastructure/OnionArchitecture.Persistence/ServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OnionArchitecture.Persistence.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArchitecture.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceService(this IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            serviceCollection.AddDbContext<ApplicationContext>(options =>
+            {
+                options.UseNpgsql(configuration.GetConnectionString("WebApiConnectionString"));
+            });
+        }
+    }
+}
